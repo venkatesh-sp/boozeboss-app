ed17940e461db48b40e96509be53cfa1f6dc0566
Irving Cabello <irvollo@gmail.com>
2020-06-01 10:03:41 -0500

    Add extra port

--- server/port.js
+++ server/port.js
@@ -1,3 +1,6 @@
 const argv = require('./argv');
 
-module.exports = process.env.NODE_ENV === 'production' ? '' : parseInt(argv.port || process.env.APP_PORT || '3000', 10);
+module.exports = parseInt(
+  argv.port || process.env.PORT || process.env.APP_PORT || '3000',
+  10,
+);
