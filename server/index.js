f22ab2281690147c79916a5045604e57f305fe1d
Irving Cabello <irvollo@gmail.com>
2020-06-01 10:16:06 -0500

    Return the old host env

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ setup(app, {
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.APP_HOST;
+const customHost = argv.host || process.env.HOST;
 const host = customHost || null; // Let http.Server use its default IPv6/4 host
 const prettyHost = customHost || 'localhost';
 
