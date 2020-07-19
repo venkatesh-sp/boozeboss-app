2a3e187677b8727b112fe68c825669ecf2a761af
Irving Cabello <irvollo@gmail.com>
2020-07-19 10:59:25 -0500

    Change wallet icon

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -126,12 +126,12 @@ module.exports = require('./webpack.base.babel')({
       ios: true,
       icons: [
         {
-          src: path.resolve('app/images/liquidintel_icon.png'),
+          src: path.resolve('app/images/512x512.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/images/liquidintel_icon.png'),
-          sizes: [120, 152, 167, 180],
+          src: path.resolve('app/images/512x512.png'),
+          sizes: [120, 152, 167, 180, 512],
           ios: true,
         },
       ],
