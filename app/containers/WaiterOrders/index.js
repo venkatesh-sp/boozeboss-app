559ea4dc38129dc7060c2135d664adaa6c8e9bf2
Preetham <preetham.varanasi@soulpageit.com>
2020-12-20 16:10:09 +0530

    Updated Ui according to the api #Preetham Varanasi

--- app/containers/WaiterOrders/index.js
+++ app/containers/WaiterOrders/index.js
@@ -71,6 +71,7 @@ class WaiterOrders extends React.Component {
   state = {
     show: false,
   };
+
   componentDidMount() {
     // console.log(this.props.history.location.state,"FROM COMPONENT DID MOUNT")
     const { user } = this.props.history.location.state;
@@ -79,73 +80,141 @@ class WaiterOrders extends React.Component {
 
   close() {
     this.setState({ show: false });
-    console.log(this.state.show, 'SHOW VARIABLE AFTE CLOSING');
+    // console.log(this.state.show, 'SHOW VARIABLE AFTE CLOSING');
   }
   open() {
     this.setState({ show: true });
-    console.log(this.state.show, 'SHOW VARIABLE AFTE Opening');
+    // console.log(this.state.show, 'SHOW VARIABLE AFTE Opening');
   }
   render() {
     // console.log(this.state.show,"SHOW VARIABLE")
-    // console.log(this.props,"PROPS IN RENDER FUNCTION");
+    // const { items } = this.props;
+    // console.log(this.props, 'PROPS IN RENDER FUNCTION');
+    // console.log(items, 'ITEMS IN RENDER FUNCTION');
     const { items } = this.props;
-    // console.log(items,"ITEMS ARRAY FROM RESPONSE IN RENDER")
+
     if (!items) {
       return <>Loading...</>;
     }
-
+    // console.log(items.items, 'ITEMS ARRAY FROM RESPONSE IN RENDER PREETHAM');
     return (
-      <div style={{ backgroundColor: '#030303', height: '100vh' }}>
-        <Helmet>
-          <title>Waiter Orders</title>
-          <meta name="description" content="Description of Waiter Orders" />
-        </Helmet>
-        <div style={{ backgroundColor: '#030303', paddingBottom: '30px' }}>
-          <StyledMenuDiv>
-            <StyledFlexContainer>
-              <StyledText size="16px" color="#ffffff" weight="bold">
-                NAME
-              </StyledText>
-              <StyledText size="13px" color="#ffffff" weight="normal" />
-            </StyledFlexContainer>
-            <StyledFlexContainer>
-              <StyledText size="16px" color="#ffffff" weight="bold">
-                TEST NAME
-              </StyledText>
-              <Button
-                appearance="primary"
-                onClick={() => {
-                  this.setState({ show: true });
-                  console.log(
-                    this.state.show,
-                    'THIS IS FROM ONCLICK MODAL BUTTON',
-                  );
-                }}
-              >
-                Modal
-              </Button>
-            </StyledFlexContainer>
-          </StyledMenuDiv>
-                {console.log(this.props,"PROPS")}
-                {console.log(this.state,"STATE")}
-          <Modal show={this.state.show} onHide={this.close}>
-            <Modal.Header>
-              <Modal.Title>Modal Title</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <Paragraph />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.close} appearance="primary">
-                Ok
-              </Button>
-              <Button onClick={this.close} appearance="subtle">
-                Cancel
-              </Button>
-            </Modal.Footer>
-          </Modal>
+      <>
+        <div style={{ backgroundColor: '#030303', height: '100vh' }}>
+          <Helmet>
+            <title>Waiter Orders</title>
+            <meta name="description" content="Description of Waiter Orders" />
+          </Helmet>
+          {items.items.map((item, index) => (
+            <div
+              style={{ backgroundColor: '#030303', paddingBottom: '30px' }}
+              key={index}
+            >
+              <StyledMenuDiv>
+                <StyledFlexContainer>
+                  <StyledText size="16px" color="#ffffff" weight="bold">
+                    {item.venueproduct[0].name}
+                  </StyledText>
+                  <StyledText size="13px" color="#ffffff" weight="normal">
+                    Rs:{item.venueproduct[0].price}
+                  </StyledText>
+                </StyledFlexContainer>
+                <StyledFlexContainer>
+                  <StyledText size="16px" color="#ffffff" weight="bold">
+                    Portfolio: {item.venueproduct[0].portfolio}
+                  </StyledText>
+                  <Button
+                    appearance="primary"
+                    onClick={() => {
+                      this.setState({ show: true });
+                    }}
+                  >
+                    Book
+                  </Button>
+                </StyledFlexContainer>
+              </StyledMenuDiv>
+            </div>
+          ))}
         </div>
-      </div>
+        <Modal
+          show={this.state.show}
+          onHide={() => this.setState({ show: false })}
+        >
+          <Modal.Header>
+            <Modal.Title>Modal Title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <label
+                htmlFor="mencount"
+                style={{ width: '100px', display: 'inline-block' }}
+              >
+                Men-Count:
+              </label>
+              <input type="number" id="mencount" name="mencount" />
+              <label htmlFor="menagegrp" style={{ marginLeft: '10px' }}>
+                Men age group:
+              </label>
+              <select
+                name="menagegrp"
+                id="menagegrp"
+                style={{ marginLeft: '33px' }}
+              >
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+              </select>
+            </div>
+            <div>
+              <label
+                htmlFor="womencount"
+                style={{ width: '100px', display: 'inline-block' }}
+              >
+                Women-Count:
+              </label>
+              <input type="number" id="womencount" name="womencount" />
+
+              <label htmlFor="womenagegrp" style={{ marginLeft: '10px' }}>
+                Women age group:
+              </label>
+              <select
+                name="womenagegrp"
+                id="womenagegrp"
+                style={{ marginLeft: '10px' }}
+              >
+                <option value="26">26</option>
+                <option value="27">27</option>
+                <option value="28">28</option>
+                <option value="29">29</option>
+                <option value="30">30</option>
+              </select>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={() => {
+                this.setState({ show: false });
+              }}
+              appearance="primary"
+            >
+              Ok
+            </Button>
+            <Button
+              onClick={() => {
+                this.setState({ show: false });
+              }}
+              appearance="subtle"
+            >
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 }
